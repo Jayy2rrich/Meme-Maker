70cc2cdf3fa20adc2207fa2e39aab07e6d037f24
Daniel <danielbonneau04@gmail.com>
2022-11-01 13:08:08 -0400

    Initial Commit

---
+++ Meme Maker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meme Maker
+//
+//  Created by 7g on 11/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
