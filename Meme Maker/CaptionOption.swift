ce39ceef4d23c47b069bd7cbcc516a9d94afe41e
Daniel <danielbonneau04@gmail.com>
2022-11-01 20:59:15 -0400

    11/1

---
+++ Meme Maker/CaptionOption.swift
@@ -0,0 +1,13 @@
+//
+//  CaptionOption.swift
+//  Meme Maker
+//
+//  Created by 7g on 11/1/22.
+//
+
+import Foundation
+
+struct CaptionOption {
+    var emoji: String
+    var caption: String
+}
--- Meme Maker/ViewController.swift
+++ Meme Maker/ViewController.swift
@@ -12,8 +12,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        topCaptionSegmentedControl.removeAllSegments()
     }
 
-
+    @IBOutlet weak var topCaptionSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var bottomCaptionSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var topCaptionLabel: UILabel!
+    
+    @IBOutlet weak var bottomCaptionLabel: UILabel!
+    
+    @IBAction func updateLabels(_ sender: Any) {
+    }
+    
+    let topChoices = [CaptionOption(emoji: "👩🏻‍❤️‍💋‍👨🏾", caption: "SHE CHEATED!?"), CaptionOption(emoji: "😳", caption: "WAIT I WON?!")]
+    
+    let bottomChoices = [CaptionOption(emoji: "😳", caption: "NO WAY!"), CaptionOption(emoji: "😳", caption: "WHAT THE F#%K!")]
+    
+   
+    
 }
 
